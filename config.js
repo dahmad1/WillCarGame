a80a8fc1a4465cb793c85b8bb7c46d39bfa68022
Noah <noah.swanson14@gmail.com>
2019-11-06 09:36:15 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+var GAME = {
+  canvas : {
+    width : 600,
+    height : 300
+  },
+  started : true,
+  level : 1
+};
+
+var CAR = {
+  initialized : false,
+  latest : {
+    x : 0,
+    y : 0
+  }
+};

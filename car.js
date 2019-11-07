8042f349c73fee7313a199c2ed33cbe8169e7006
Noah <noah.swanson14@gmail.com>
2019-11-06 21:51:52 -0800

    Update car.js

--- car.js
+++ car.js
@@ -3,27 +3,28 @@ function InitializeCar(){
   var context = canvas.getContext('2d');
   context.scale(1,1);
   CAR = {
-    x : 300,
-    y : 300,
+    x : 100,
+    y : 100,
     latest : {
         x : CAR.x,
         y : CAR.y,
     },
     scale : 8,
     speed : 3,
-
+    initialized : true
   }
-
-  function RenderCar(context) {
-  if (!CAR.initialized) {
-    return;
-  }
-  context.moveTo(CAR.x,CAR.y);
-  CAR.latest.x = CAR.x;
-  CAR.latest.y = CAR.y;
-  context.beginPath();
-  context.fillRect(CAR.x, CAR.y, 50, 50);
-  context.stroke();
-
 }
+  function renderCar(context) {
+    if (!CAR.initialized) {
+      return;
+    }
+    context.moveTo(CAR.x,CAR.y);
+    CAR.latest.x = CAR.x;
+    CAR.latest.y = CAR.y;
+    context.beginPath();
+    context.rect(CAR.x, CAR.y, 50, 50);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+
 }

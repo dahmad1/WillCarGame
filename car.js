e9c03d0da5371cf501f59af92e04cb0728caf0c0
VeritableSmith <chasevanamburg@Chases-MacBook-Pro-2.local>
2019-11-24 07:40:58 -0800

    Updated car.js
    -Cleaned up the Render Function
    -Changed car and screen movement
    -Created collateral damage display

--- car.js
+++ car.js
@@ -1,3 +1,5 @@
+var redCar = new Image(); redCar.src="Images/spr_red_car.png";
+
 function InitializeCar(){
   var canvas = document.getElementById('mainCanvas');
   var context = canvas.getContext('2d');
@@ -11,20 +13,109 @@ function InitializeCar(){
     },
     scale : 8,
     speed : 3,
-    initialized : true
+    initialized : true,
+    speedX : 0,
+    MAX_SPEED_X : 3,
+    speedY : 0,
+    MAX_SPEED_Y : 2,
+    distanceTraveled : 0,
+    collateralDamage : 0
   }
 }
-  function renderCar(context) {
-    if (!CAR.initialized) {
-      return;
-    }
-    context.moveTo(CAR.x,CAR.y);
-    CAR.latest.x = CAR.x;
-    CAR.latest.y = CAR.y;
-    context.beginPath();
-    context.rect(CAR.x, CAR.y, 50, 50);
-    context.fillStyle = "red";
-    context.fill();
-    context.closePath();
 
+function handleCarAnimation() {
+
+  if (CONTROLS.car.forward) {
+    if (CONTROLS.fire.active) {
+      CAR.y -= 10;
+    }else
+    {
+      if(CAR.speedY>-CAR.MAX_SPEED_Y) {CAR.speedY -=  0.05;}
+    }
+  }
+  else if (CONTROLS.car.backward) {
+    if (CONTROLS.fire.active) {
+      CAR.y += 10;
+    }else
+    {
+      if(CAR.speedY<CAR.MAX_SPEED_Y){CAR.speedY +=  0.05;}
+    }
+  }
+  else {
+    if(CAR.speedY>0) {CAR.speedY-=0.025;}
+    else if(CAR.speedY<0) {CAR.speedY+=0.025;}
+    if(Math.abs(CAR.speedY)<0.1) {CAR.speedY=0;}
+  }
+
+
+  if (CONTROLS.car.right) {
+    if (CONTROLS.fire.active) {
+      CAR.x += 10;
+    }else
+    {
+      if(CAR.speedX<CAR.MAX_SPEED_X){CAR.speedX +=  0.05;}
+      if(ART.linespeed<ART.MAX_LINESPEED){ART.linespeed +=  0.05;}
+    }
+  }
+  else if (CONTROLS.car.left) {
+    if (CONTROLS.fire.active) {
+      CAR.x -= 10;
+    }else
+    {
+      if(CAR.speedX>-CAR.MAX_SPEED_X){CAR.speedX -=  0.2;}
+      if(ART.linespeed>0){ART.linespeed -=  0.01;}
+
+    }
+  }
+  else if (CONTROLS.car.brake){
+    if(CAR.speedX>0) {CAR.speedX-=0.1;}
+    else if(CAR.speedX<0) {CAR.speedX+=0.1;}
+
+    if(ART.linespeed>0) {
+      ART.linespeed-=.1;
+
+    }
+  }
+  else {
+    if(CAR.speedX>0) {CAR.speedX-=0.05;}
+    else if(CAR.speedX<0) {CAR.speedX+=0.05;}
+    if(ART.linespeed>0) {ART.linespeed-=.01;}
+  }
+
+  if(CAR.y<=40||CAR.y>=GAME.canvas.height-40) {
+    CAR.speedX-=.1;
+    ART.linespeed-=.1;
+  }
+
+  if(ART.linespeed<0){ART.linespeed=0;}
+  if(ART.linespeed>ART.MAX_LINESPEED){ART.linespeed=ART.MAX_LINESPEED;}
+
+  CAR.x +=  CAR.speedX;
+  CAR.y +=  CAR.speedY;
+  CAR.distanceTraveled+=ART.linespeed;
+
+  if(CAR.y+25>GAME.canvas.height) {CAR.y=GAME.canvas.height-25;}
+  else if(CAR.y-25<0) {CAR.y=25}
+
+  if (CAR.x > GAME.canvas.width-125) {
+    CAR.x = GAME.canvas.width-125;
+  } else if (CAR.x < 50) {
+    CAR.x = 50;
+  }
+}
+
+
+function renderCar(context) {
+    context.drawImage(redCar, CAR.x-27.5, CAR.y-22.5, 55, 45);
+}
+
+function displayCollateralDamage(context) {
+  context.fillStyle='white';
+  context.fillText("Collateral Damage: " +CAR.collateralDamage+"/50", 10, GAME.canvas.height-10);
+}
+
+function setCollateralDamage() {
+  CAR.MAX_SPEED_X=0.9*CAR.MAX_SPEED_X;
+  CAR.MAX_SPEED_Y=1.2*CAR.MAX_SPEED_Y;
+  ART.MAX_LINESPEED=0.9*ART.MAX_LINESPEED;
 }
